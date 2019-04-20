f1564e69e93d411b26c317b9b8eb7852d58d4a7b
Fabian Zabala <fabiandzp@live.com>
2019-04-20 18:56:56 -0500

    fix store id

--- tests/system/store_test.py
+++ tests/system/store_test.py
@@ -17,7 +17,7 @@ class StoreTest(BaseTest):
                 r = c.get('/store/test')
 
                 self.assertEqual(r.status_code, 200)
-                self.assertDictEqual(d1={'id': None, 'name': 'test', 'items': []},
+                self.assertDictEqual(d1={'id': 1, 'name': 'test', 'items': []},
                                      d2=json.loads(r.data))
 
     def test_store_with_items_found(self):
